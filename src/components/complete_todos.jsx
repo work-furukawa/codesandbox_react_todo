a37b0b89a6d8e9006b350afc705640443c19b2a1
work-furukawa <46434770+work-furukawa@users.noreply.github.com>
2021-05-03 13:46:52 +0900

    todo(react)

---
+++ src/components/complete_todos.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+export const CompleteTodos = (props) => {
+  const { todos, onClickBack } = props;
+
+  return (
+    <div className="complete-area">
+      <p className="title">完了のTODO</p>
+      <ul>
+        {todos.map((todo, index) => {
+          return (
+            <li>
+              <div>
+                {todo}
+                <button onClick={() => onClickBack(index)}>戻す</button>
+              </div>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
